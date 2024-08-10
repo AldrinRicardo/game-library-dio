b543c2bd7a165e486faa5abd875a0a6124377b69
Aldrin Ricardo <aldrinricardo96@gmail.com>
2024-08-10 12:07:42 -0300

    Tags e descrições para Swagger UI adicionadas

--- src/main/java/me/dio/controller/GameController.java
+++ src/main/java/me/dio/controller/GameController.java
@@ -14,11 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import me.dio.model.Game;
 import me.dio.service.GameService;
 
 @RestController
 @RequestMapping("/games")
+@Tag(name = "Games Controller", description = "RESTful API for managing games")
 public class GameController {
 
 	private final GameService gameService;
@@ -29,17 +32,20 @@ public class GameController {
 	}
 	
 	@GetMapping
+	@Operation(summary = "Get all games", description = "Retrieves a list of all registered games")
 	public ResponseEntity<List<Game>> findAll(){	
 		return ResponseEntity.ok().body(gameService.findAll());
 	}
 	
 	@GetMapping("/{id}")
+	@Operation(summary = "Get a game by ID", description = "Retrieves a specific game based on its ID")
 	public ResponseEntity<Game> findById(@PathVariable Long id){
 		Game game = gameService.findById(id);
 		return ResponseEntity.ok(game);
 	}
 	
 	@PostMapping
+	@Operation(summary = "Save a new game", description = "Saves a new game and return its data")
 	public ResponseEntity<Game> save(@RequestBody Game gameToSave){
 		Game gameSaved = gameService.save(gameToSave);
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(gameSaved.getId()).toUri();
@@ -47,12 +53,14 @@ public class GameController {
 	}
 	
 	@PutMapping("/{id}")
+	@Operation(summary = "Update a game", description = "Updates an existing game data based on its ID")
 	public ResponseEntity<Game> update(@PathVariable Long id, @RequestBody Game game) {
         gameService.update(id, game);
         return ResponseEntity.ok(game);
     }
 	
 	@DeleteMapping("/{id}")
+	@Operation(summary = "Delete a game", description = "Deletes an existing game based on its ID")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		gameService.delete(id);
 		return ResponseEntity.noContent().build();
--- src/main/java/me/dio/controller/PlatformController.java
+++ src/main/java/me/dio/controller/PlatformController.java
@@ -12,11 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import me.dio.model.Platform;
 import me.dio.service.PlatformService;
 
 @RestController
 @RequestMapping("/platforms")
+@Tag(name = "Platform Controller", description = "RESTful API for managing gaming platforms")
 public class PlatformController {
 
 	private final PlatformService platformService;
@@ -27,12 +30,14 @@ public class PlatformController {
 	}
 	
 	@GetMapping("/{id}")
+	@Operation(summary = "Get a platform by ID", description = "Retrieves a specific platform based on its ID")
 	public ResponseEntity<Platform> findById(@PathVariable Long id){
 		Platform platform = platformService.findById(id);
 		return ResponseEntity.ok(platform);
 	}
 	
 	@PostMapping
+	@Operation(summary = "Save a new platform", description = "Saves a new platform and return its data")
 	public ResponseEntity<Platform> save(@RequestBody Platform platformToSave){
 		Platform platformSaved = platformService.save(platformToSave);
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(platformSaved.getId()).toUri();
@@ -40,6 +45,7 @@ public class PlatformController {
 	}
 	
 	@DeleteMapping("/{id}")
+	@Operation(summary = "Delete a platform", description = "Deletes a platform based on its ID")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		platformService.delete(id);
 		return ResponseEntity.noContent().build();
